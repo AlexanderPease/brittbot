a2d1f5a46320606f6bc11c6952c6daf915ec193e
AlexanderPease <alexanderpease@usv.com>
2013-08-17 14:02:12 -0400

    Everything should work except the database is being weird on Heroku and brittany@usv.com isn't sending emails

--- intro/urls.py
+++ intro/urls.py
@@ -3,4 +3,5 @@ from intro import views
 
 urlpatterns = patterns('brittbot.views',
     url(r'^$', views.index, name='index'),
+    url(r'^response/(?P<intro_id>[0-9]+)/$', views.response, name='response'),
 )
--- intro/views.py
+++ intro/views.py
@@ -8,7 +8,9 @@ from django.core.mail import send_mail
 from intro.models import Intro, IntroForm
 import datetime
 
+RESPONSE_URL = 'http://brittbot.herokuapp.com/response/'
 
+# Form for Brittany to fill out. Sends initial email request.
 def index(request):
 	if request.method == 'POST':
 		print 'POST'
@@ -20,8 +22,10 @@ def index(request):
 			# Send initial email
 			try:
 				email_subject = "Intro to %s?" % intro.for_name
-				email_body = 'Hi %s, %s wants to meet with you to discuss %s. If you are open to the connection please <a href="#">click here</a>. If someone else should take this, please insert TBD' % (intro.to_name, intro.for_name, intro.purpose) 
+				email_body = 'Hi %s, %s wants to meet with you to discuss %s. If you are open to the connection please <a href="%s%s">click here</a>.' % (intro.to_name, intro.for_name, intro.purpose, RESPONSE_URL, intro_id) 
 				send_mail(email_subject, email_body, EMAIL_HOST_USER, [intro.to_email], fail_silently=False)
+				intro.sent = datetime.date.today()
+				intro.save()
 				return redirect('/?sent=%s' % intro.to_name) # Always redirect after successful POST
 			except:
 				intro.delete()
@@ -34,5 +38,21 @@ def index(request):
 	else:
 		form = IntroForm
 		sent = request.GET.get('sent')
-		print 'GET'
 		return render_to_response('index.html', {'form': form, 'sent': sent}, context_instance=RequestContext(request))
+
+# Handles response from the initial email.
+def response(request, intro_id):
+	if request.method == 'POST':
+		try:
+			intro = Intro.objects.get(id=intro_id)
+			email_subject = "%s <-> %s" % (intro.for_name, intro.to_name)
+			email_body = 'Great that you guys are connecting!'
+			send_mail(email_subject, email_body, EMAIL_HOST_USER, [intro.to_email], fail_silently=False)
+			intro.connected = datetime.date.today()
+			intro.save()
+			return render_to_response('response.html', {'intro': intro}, context_instance=RequestContext(request))
+		else:
+			return render_to_response('response.html', {'error': 'Intro could not be sent, please reload the page or contact brittany@usv.com'}, context_instance=RequestContext(request))
+	else:
+		return render_to_response('response.html', {'error': 'Page should not be loaded by GET'}, context_instance=RequestContext(request))
+	
