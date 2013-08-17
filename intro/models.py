0ada203d37521043b240df139304b04b24a49769
AlexanderPease <alexanderpease@usv.com>
2013-08-17 13:39:01 -0400

    Finished form but emails not sending from brittany@usv.com. Pushing up to heroku to try there

--- intro/models.py
+++ intro/models.py
@@ -7,7 +7,7 @@ class Intro(models.Model):
     to_email = models.EmailField(max_length=75, unique=False)
     for_name = models.CharField(max_length=75, unique=False)
     for_email = models.EmailField(max_length=75, unique=False)
-    purpose = models.CharField(max_length=500, unique=False)
+    purpose = models.TextField(max_length=500, unique=False)
     sent = models.DateField(blank=True, null=True, unique=False) # Date the first email was sent
     connected = models.DateField(blank=True, null=True, unique=False) # Date the connection email was sent to both people
 
@@ -26,6 +26,8 @@ class IntroForm(ModelForm):
         self.fields['to_email'].widget.attrs = {'class': 'form-control', 'placeholder': 'Email'}
         self.fields['for_name'].widget.attrs = {'class': 'form-control', 'placeholder': 'Person asking for the intro'}
         self.fields['for_email'].widget.attrs = {'class': 'form-control', 'placeholder': 'Email'}
+        self.fields['purpose'].widget.attrs = {'class': 'form-control', 'placeholder': 'Why he/she wants to connect', 'rows': '4'}
+
 
     
     
