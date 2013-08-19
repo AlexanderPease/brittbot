e06da14aeb8309af48ea62ff9fe2d710e4cf8360
AlexanderPease <alexanderpease@usv.com>
2013-08-19 18:33:16 -0400

    Working prototype

--- intro/views.py
+++ intro/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import Context, RequestContext, loader
 from django.conf import settings #for STATIC_URL
 from django.core.context_processors import csrf
+from django.core.mail import EmailMultiAlternatives
 from django.core.mail import send_mail
 #import os.path # to find static files
 from intro.models import Intro, IntroForm
 import datetime
 
-RESPONSE_URL = "http://brittbot.herokuapp.com/response/"
+RESPONSE_URL = 'http://127.0.0.1:8000/response/'#"http://brittbot.herokuapp.com/response/"
 
-# Form for Brittany to fill out. Sends initial email request.
+'''
+Form for Brittany to fill out. Sends initial email request.
+'''
 def index(request):
 	if request.method == 'POST':
 		print 'POST'
@@ -21,13 +24,18 @@ def index(request):
 			# Send initial email
 			try:
 				email_subject = "Intro to %s?" % intro.for_name
-				email_body = 'Hi %s, %s wants to meet with you to discuss %s. If you are open to the connection please <a href="%s%s">click here</a>.' % (intro.to_name, intro.for_name, intro.purpose, RESPONSE_URL, intro.id) 
+				text_body = 'Hi %s, %s wants to meet with you to discuss %s. If you are open to the connection please email reply to brittany@usv.com.' % (intro.to_name, intro.for_name, intro.purpose)
+				html_body = 'Hi %s, %s wants to meet with you to discuss %s. If you are open to the connection please <a href="%s%s">click here</a>.' % (intro.to_name, intro.for_name, intro.purpose, RESPONSE_URL, intro.id)
+				
 				print "sending to: %s" % intro.to_email
 				print "subject: %s" % email_subject
-				print "body: %s" % email_body
+				print "body: %s" % text_body
 				print "host: %s" % settings.EMAIL_HOST_USER
 
-				send_mail(email_subject, email_body, settings.EMAIL_HOST_USER, [intro.to_email], fail_silently=False)
+				msg = EmailMultiAlternatives(email_subject, text_body, settings.EMAIL_HOST_USER, [intro.to_email])
+				msg.attach_alternative(html_body, "text/html")
+				msg.send()
+
 				print "email sent"
 				intro.sent = datetime.date.today()
 				intro.save()
@@ -45,19 +53,34 @@ def index(request):
 		sent = request.GET.get('sent')
 		return render_to_response('index.html', {'form': form, 'sent': sent}, context_instance=RequestContext(request))
 
-# Handles response from the initial email.
+'''
+Handles response from the initial email.
+Currently, the link emailed out modifies the database with a GET request, which is not ideal.
+But there is no way to let most email client include a POST link (and for good reason). 
+'''
 def response(request, intro_id):
-	if request.method == 'POST':
-		try:
-			intro = Intro.objects.get(id=intro_id)
-			email_subject = "%s <-> %s" % (intro.for_name, intro.to_name)
-			email_body = 'Great that you guys are connecting!'
-			send_mail(email_subject, email_body, EMAIL_HOST_USER, [intro.to_email], fail_silently=False)
-			intro.connected = datetime.date.today()
-			intro.save()
-			return render_to_response('response.html', {'intro': intro}, context_instance=RequestContext(request))
-		except:
-			return render_to_response('response.html', {'error': 'Intro could not be sent, please reload the page or contact brittany@usv.com'}, context_instance=RequestContext(request))
-	else:
-		return render_to_response('response.html', {'error': 'Page should not be loaded by GET'}, context_instance=RequestContext(request))
-	
+	try:
+		intro = Intro.objects.get(id=intro_id)
+	except:
+		return render_to_response('response.html', {'error': "Sorry! I couldn't find this intro in my database. Please contact brittany@usv.com"}, context_instance=RequestContext(request))
+
+	if intro.connected is not None:
+		return render_to_response('response.html', {'error': "Your email has already been sent! Stop clicking the link!", 'intro': intro}, context_instance=RequestContext(request))
+
+	email_subject = "%s <-> %s" % (intro.for_name, intro.to_name)
+	email_body = 'Great that you guys are connecting!'
+	send_mail(email_subject, email_body, settings.EMAIL_HOST_USER, [intro.to_email], fail_silently=False)
+	intro.connected = datetime.date.today()
+	intro.save()
+	return render_to_response('response.html', {'intro': intro}, context_instance=RequestContext(request))
+
+
+
+
+
+
+
+
+
+
+
